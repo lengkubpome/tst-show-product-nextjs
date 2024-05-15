98c439a2e326311bb041601ddca8052b0f847f1c
lengkubpome <lengkubpome@gmail.com>
2024-05-16 00:34:58 +0700

    พร้อมสำหรับ deploy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -33,6 +33,7 @@ interface ProductCurrentPrice {
               addonPrice: number;
               startDate: string;
               endDate: string;
+              type: string,
               status: string;
             }
           ]
@@ -78,27 +79,28 @@ export default function Home() {
       ) : (
         <div className="z-10 max-w-5xl w-full items-center justify-between font-semibold text-sm lg:flex">
 
-          <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+          <p className="fixed left-0 top-0 flex w-full justify-center text-xl border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
             ราคารับซื้อ เริ่ม&nbsp;
             <time suppressHydrationWarning >{new Date(productData!.productPriceChange.changeDate).toLocaleDateString('th-TH')}</time>
           </p>
 
-          <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+          <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t text-sky-700 from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
             <a
-              className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+              className="pointer-events-none flex place-items-baseline gap-2 p-8 lg:pointer-events-auto lg:p-0"
               href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
               target="_blank"
               rel="noopener noreferrer"
             >
               By{' '}
-              <Image
+              <p className='text-2xl'>ไทยสุทัศน์ <br />รีไซเคิล</p>
+              {/* <Image
                 src="/vercel.svg"
                 alt="Vercel Logo"
                 className="dark:invert"
                 width={100}
                 height={24}
                 priority
-              />
+              /> */}
             </a>
           </div>
         </div>
@@ -106,12 +108,12 @@ export default function Home() {
 
       <div className="bg-white sm:py-10">
         {productData?.productCategories.filter((category) => category.status == "Active").map((category) => (
-          <div key={category.id} className="mx-auto mb-4 max-w-7xl px-6 lg:px-8 py-10 ">
+          <div key={category.id} className="mx-auto mb-4 max-w-7xl px-6 lg:px-8 py-6 ">
             <div className="mx-auto max-w-2xl lg:text-center">
-              <h2 className="text-2xl font-semibold leading-7 text-indigo-600">{category.name}</h2>
+              <h2 className="text-2xl font-semibold leading-7 text-sky-700">{category.name}</h2>
 
             </div>
-            <div className="mx-auto mt-8 max-w-2xl sm:mt-10 lg:mt-14 lg:max-w-4xl ">
+            <div className="mx-auto mt-8 max-w-2xl sm:mt-8  lg:max-w-4xl ">
               <dl className="grid max-w-xl grid-cols-1 gap-x-4 gap-y-4 lg:max-w-none lg:grid-cols-2 ">
                 {category.productList.map((product) => (
                   // <div key={product.name} className="relative pl-16 ">
@@ -142,11 +144,11 @@ export default function Home() {
                     </dt>
                     {product.promotionList?.map((promotion) => (
 
-                      <dd key={promotion.order} className="mt-2 text-base leading-7 flex justify-between text-gray-600">
+                      <dd key={promotion.order} className={`mt-2 text-base leading-7 flex justify-between text-gray-600 ${promotion.type == 'forbidden' ? 'text-red-500' : ''}`}>
 
                         <div className='flex items-center justify-center align-middle'>
-                          <HashtagIcon className="h-4 w-4 pr-1" aria-hidden="true" />
-                          {promotion.description}
+                          {/* <HashtagIcon className="h-4 w-4 pr-1" aria-hidden="true" /> */}
+                          # {promotion.description}
                         </div>
                         {promotion.addonPrice != 0 &&
                           <div>
